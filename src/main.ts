0a20b44979f1bdb7b2d7e6cd379bcbcf425fbc88
Taiwo kazeem <kazeem@kobo360.com>
2025-10-13 20:10:26 +0100

    refactor(main): streamline worker initialization and improve SSL configuration comments

--- src/main.ts
+++ src/main.ts
@@ -7,36 +7,43 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  if (process.argv.includes('--worker')) {
+    // Start only the Bull worker
+    const app = await NestFactory.createApplicationContext(AppModule);
+    await app.init();
+    console.log('NestJS Worker started.');
+  } else {
+    const app = await NestFactory.create(AppModule);
 
-   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    // skipMissingProperties: true,
-  }));
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      // skipMissingProperties: true,
+    }));
 
-  app.enableCors();
+    app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
-    .setVersion('0.1')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('Median')
+      .setDescription('The Median API description')
+      .setVersion('0.1')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
 
-  app.use(
-    session({
-      secret: process.env.SECRET, // use a strong secret in production!
-      resave: false,
-      saveUninitialized: false,
-      cookie: { secure: process.env.NODE_ENV === 'production' ? true : false }, // set to true if using HTTPS
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
+    app.use(
+      session({
+        secret: process.env.SECRET, // use a strong secret in production!
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: process.env.NODE_ENV === 'production' ? true : false }, // set to true if using HTTPS
+      }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
+  }
 }
 bootstrap();
